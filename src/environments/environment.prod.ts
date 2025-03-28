3f010801c3d942945d070a2a9cef78e41eb8b9da
frankaong <belleisaong@gmail.com>
2025-03-28 14:46:24 +0800

    updated html

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  API_KEY: '',
+  API_URL_ONECALL: 'https://api.openweathermap.org/data/3.0/onecall',
+  API_URL_WEATHER: 'https://api.openweathermap.org/data/2.5/weather'
+};
