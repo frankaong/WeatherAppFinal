8a5039c1223253352407b9027c07661a0dd5b047
frankaong <belleisaong@gmail.com>
2025-03-28 14:34:55 +0800

    Removed environment files from Git tracking

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  API_KEY: '2b805e9ef65a65502a47706b48242d77',
-  API_URL_ONECALL: 'https://api.openweathermap.org/data/3.0/onecall',
-  API_URL_WEATHER: 'https://api.openweathermap.org/data/2.5/weather'
-};
